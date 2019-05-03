14e36bdc72b18169f1341e732356fb84f7c1c37f
simplon-marseille <acardnicolas@hotmail.com>
2019-05-03 12:16:30 +0200

    rerereererreretest

--- src/dynamicForm.js
+++
@@ -1,9 +0,0 @@
-const formulaire = document.querySelector('form input');
-
-formulaire.addEventListener("mouseover", (event) => {
-  formulaire.classList.add('form-xl');
-});
-
-formulaire.addEventListener("mouseout", (event) => {
-  formulaire.classList.remove('form-xl');
-});
